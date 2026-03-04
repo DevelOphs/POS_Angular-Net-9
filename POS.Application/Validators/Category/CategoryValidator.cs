fe7a7c188a8fda14f2e221434474271e84c6b0a6
DevelOphs <fracordero@est.ecotec.edu.ec>
2026-03-03 22:54:02 -0500

    CRUD en category, Validators, DTOs, Filtros y metodos en interfaz CategoryRepository

---
+++ POS.Application/Validators/Category/CategoryValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using POS.Application.Dtos.Request;
+
+namespace POS.Application.Validators.Category
+{
+    public class CategoryValidator : AbstractValidator<CategoryRequestDto>
+    {
+        public CategoryValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotNull().WithMessage("El nombre de la categoría es requerido.")
+                .NotEmpty().WithMessage("El nombre de la categoría no puede estar vacío.")
+                .MaximumLength(100).WithMessage("El nombre de la categoría no puede exceder los 100 caracteres.");
+            RuleFor(x => x.Description)
+                .MaximumLength(500).WithMessage("La descripción de la categoría no puede exceder los 500 caracteres.");
+            RuleFor(x => x.State)
+                .InclusiveBetween(0, 1).WithMessage("El estado de la categoría debe ser 0 (inactivo) o 1 (activo).");
+        }
+    }
+}
---
+++ POS.Infrastructure/Persistences/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿namespace POS.Infrastructure.Persistences.Interfaces
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        //Declaracion o matricula de nuestras interfaces de repositorios
+        ICategoryRespository Category { get; }
+
+        void SaveChanges();
+        Task SaveChangesAsync();
+    }
+}
