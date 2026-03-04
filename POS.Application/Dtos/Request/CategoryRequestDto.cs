e4911a1c62998e9acbd1070058c59382eefff95d
DevelOphs <fracordero@est.ecotec.edu.ec>
2026-03-03 22:54:02 -0500

    Descripción del cambio

---
+++ POS.Application/Dtos/Request/CategoryRequestDto.cs
@@ -0,0 +1,11 @@
+﻿using POS.Domain.Entities;
+
+namespace POS.Application.Dtos.Request
+{
+    public class CategoryRequestDto
+    {
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public int State { get; set; }
+    }
+}
---
+++ POS.Infrastructure/Persistences/Repositories/CategoryRepository.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.VisualBasic;
+using POS.Domain.Entities;
+using POS.Infrastructure.Commons.Bases.Request;
+using POS.Infrastructure.Commons.Bases.Response;
+using POS.Infrastructure.Persistences.Contexts;
+using POS.Infrastructure.Persistences.Interfaces;
+using POS.Utilities.Static;
+using System.Linq.Dynamic.Core;
+
+namespace POS.Infrastructure.Persistences.Repositories
+{
+    public class CategoryRepository : GenericRepository<Category>, ICategoryRespository
+    {
+        private readonly POSContext _context;
+
+        public CategoryRepository(POSContext context)
+        {
+            _context = context;
+        }
+        public async Task<BaseEntityResponse<Category>> ListCategories(BaseFilterRequest filters)
+        {
+            var response = new BaseEntityResponse<Category>();
+            var categories = (from c in _context.Categories
+                              where c.AuditDeleteDate == null && c.AuditDeleteDate == null
+                              select c).AsNoTracking().AsQueryable();
+
+            if(filters.NumFilter is not null && !string.IsNullOrEmpty(filters.TextFilter))
+            {
+                switch (filters.NumFilter)
+                {
+                    case 1:
+                        categories = categories.Where(x => x.Name!.Contains(filters.TextFilter));
+                        break;
+                    case 2:
+                        categories = categories.Where(x => x.Description!.Contains(filters.TextFilter));
+                        break;
+                }
+            }
+            if (filters.StateFilter is not null)
+            {
+                categories = categories.Where(x => x.State.Equals(filters.StateFilter));
+            }
+
+            if (!string.IsNullOrEmpty(filters.StartDate) && !string.IsNullOrEmpty(filters.EndDate))
+            {
+                categories = categories.Where(x => x.AuditCreateDate >= Convert.ToDateTime(filters.StartDate) && x.AuditCreateDate <= Convert.ToDateTime(filters.EndDate).AddDays(1));
+            }
+
+            if (filters.Sort is null) filters.Sort = "CategoryId";
+
+            response.TotalRecords = await categories.CountAsync();
+            response.Items = await Ordering(filters, categories, !(bool)filters.Download!).ToListAsync();
+            return response;
+        }
+        public async Task<IEnumerable<Category>> ListSelectCategories()
+        {
+            var categories = await _context.Categories
+                .Where(x => x.State.Equals((int)StateTypes.Active) && x.AuditDeleteDate == null && x.AuditDeleteDate == null).AsNoTracking().ToListAsync();
+            return categories;
+        }
+        public async Task<Category> CategoryById(int categoryid)
+        {
+            var category = await _context.Categories!.AsNoTracking().FirstOrDefaultAsync(x=>x.CategoryId.Equals(categoryid));
+            return category!;
+        }
+        public async Task<bool> RegisterCategory(Category category)
+        {
+            category.AuditCreateUser = 1;
+            category.AuditCreateDate = DateTime.Now;
+
+            await _context.AddAsync(category);
+
+            var recordsAffected = await _context.SaveChangesAsync();
+            return recordsAffected > 0;
+        }
+        public async Task<bool> EditCategory(Category category)
+        {
+            category.AuditUpdateUser = 1;
+            category.AuditUpdateDate = DateTime.Now;
+
+            _context.Update(category);
+            _context.Entry(category).Property(x => x.AuditCreateUser).IsModified = false;
+            _context.Entry(category).Property(x => x.AuditCreateDate).IsModified = false;
+            var recordsAffected = await _context.SaveChangesAsync();
+            return recordsAffected > 0;
+        }
+        public async Task<bool> RemoveCategory(int categoryid)
+        {
+            var category = await _context.Categories.AsNoTracking().SingleOrDefaultAsync(x => x.CategoryId.Equals(categoryid));
+
+            category!.AuditDeleteUser = 1;
+            category.AuditDeleteDate = DateTime.Now;
+
+            var recordsAffected = await _context.SaveChangesAsync();
+            return recordsAffected >0;
+        }
+    }
+}
