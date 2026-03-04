61ce5e21d1bb00582ea454587b7c21d9be49afc0
DevelOphs <fracordero@est.ecotec.edu.ec>
2026-03-03 22:54:02 -0500

    CRUD en category, Validators, DTOs, Filtros y metodos en interfaz CategoryRepository

---
+++ POS.Infrastructure/Persistences/Interfaces/ICategoryRespository.cs
@@ -0,0 +1,16 @@
+﻿using POS.Domain.Entities;
+using POS.Infrastructure.Commons.Bases.Request;
+using POS.Infrastructure.Commons.Bases.Response;
+
+namespace POS.Infrastructure.Persistences.Interfaces
+{
+    public interface ICategoryRespository
+    {
+        Task<BaseEntityResponse<Category>> ListCategories(BaseFilterRequest filters);
+        Task<IEnumerable<Category>> ListSelectCategories();
+        Task<Category> CategoryById(int categoryid);
+        Task<bool> RegisterCategory(Category category);
+        Task<bool> EditCategory(Category category);
+        Task<bool> RemoveCategory(int categoryid);
+    }
+}
