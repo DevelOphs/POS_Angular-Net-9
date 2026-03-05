1a60188a2632aa0e3fc0b415f9bb09dc55bec914
DevelOphs <fracordero@est.ecotec.edu.ec>
2026-03-05 00:19:19 -0500

    fix: Corregir constructor de CategoryRepository para pasar DbSet requerido
    - Se agregó el segundo parámetro (context.Categories) al constructor base
    - Esto resuelve el error CS7036 sobre parámetro faltante 'entity'
    - El GenericRepository requiere tanto POSContext como DbSet<T>

---
+++ POS.Test/Category/CategoryApplicationTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace POS.Test.Category
+{
+    [TestClass]
+    public class CategoryApplicationTest
+    {
+        private static WebApplicationFactory<Program>? _factory = null;
+        private static IServiceScopeFactory? _scopeFactory = null;
+
+        [ClassInitialize]
+        public static void Initialize(TestContext _testContext)
+        {
+            _factory = new CustomWebApplicationFactory();
+            _scopeFactory = _factory.Services.GetService<IServiceScopeFactory>();
+        }
+
+    }
+}
